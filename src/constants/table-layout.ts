aa8f529ec038efa91b4c4d1f7731eb087552c380
balongbesuk <desa.balongbesuk@gmail.com>
2026-05-06 23:57:06 +0700

    fix: increase wilayah column width to prevent blok text truncation

--- src/constants/table-layout.ts
+++ src/constants/table-layout.ts
@@ -6,11 +6,11 @@ export const TAX_TABLE_WIDTHS = {
   checkbox: "w-[50px] shrink-0",
   nop: "w-[180px] shrink-0",
   wpInfo: "flex-1 min-w-[300px] shrink-0", // Includes Nama WP & Alamat
-  wilayah: "w-[150px] shrink-0",
+  wilayah: "w-[180px] shrink-0",
   tagihan: "w-[130px] shrink-0",
   status: "w-[120px] shrink-0",
   penarik: "w-[150px] shrink-0",
-  minContainerWidth: "md:min-w-[1100px]",
+  minContainerWidth: "md:min-w-[1150px]",
 };
 
 export type TaxColumnKey = keyof typeof TAX_TABLE_WIDTHS;
