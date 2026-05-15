8955e5261b1067692568b614d9736a854651b4a7
balongbesuk <desa.balongbesuk@gmail.com>
2026-05-15 22:29:31 +0700

    chore: remove temporary scripts from repository

--- inject_names.ps1
+++
@@ -1,8 +0,0 @@
-$dir = "F:\Projek Vibe Koding\pbb-app-laravel\map-data"
-Get-ChildItem "$dir\*.gpx" | ForEach-Object {
-    $name = $_.BaseName
-    $content = Get-Content $_.FullName -Raw -Encoding UTF8
-    $newContent = $content -replace '<name></name>', "<name>$name</name>"
-    [System.IO.File]::WriteAllText($_.FullName, $newContent, [System.Text.UTF8Encoding]::new($false))
-    Write-Output "OK: $($_.Name) -> $name"
-}
--- scratch/install_breeze.ps1
+++
@@ -1,5 +0,0 @@
-$phpPath = "F:\laragon\bin\php\php-8.3.30-Win32-vs16-x64"
-$composerPath = "F:\laragon\bin\composer"
-$env:PATH = "$phpPath;$composerPath;" + $env:PATH
-cd "F:\Projek Vibe Koding\pbb-app-laravel\web-laravel"
-php artisan breeze:install react --dark --typescript --eslint --no-interaction
